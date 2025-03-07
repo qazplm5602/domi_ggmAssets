5756ae9c24500cc07d5b75146ddde614acbb4762
domi <domi@domi.kr>
2025-03-07 19:53:32 +0900

    클라이언트 init222

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  },
+  resolve: {
+    alias: [
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') }
+    ]
+  }
 })
