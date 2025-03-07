dacd004ca7419cff7411d684e17d767e5396d009
domi <domi@domi.kr>
2025-03-08 04:08:10 +0900

    backend init

---
+++ server/src/main/java/com/domi/ggmassetbackend/GgmAssetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domi.ggmassetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgmAssetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgmAssetBackendApplication.class, args);
+    }
+
+}
