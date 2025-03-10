d28e037772b8a8e9fbccbe26e8a639753dfdf8c8
domi <domi@domi.kr>
2025-03-10 23:50:31 +0900

    프론트 검색 박스

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import style from '@styles/header/style.module.scss';
 import HeaderInteraction from './Interaction';
 import HeaderMain from './Main';
+import SearchBox from '@components/SearchBox/SearchBox';
 
 export default function Header() {
     return <header className={style.main}>
         <HeaderMain />
+        <SearchBox className={style.search} />
         <HeaderInteraction />
     </header>;
 }
