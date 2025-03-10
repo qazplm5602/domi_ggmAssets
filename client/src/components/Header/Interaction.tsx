752312204c6dc5049d463f3157043f8d7e572713
domi <domi@domi.kr>
2025-03-10 20:32:24 +0900

    프론트 헤더 만드는중

--- client/src/components/Header/Interaction.tsx
+++ client/src/components/Header/Interaction.tsx
@@ -1,7 +1,11 @@
 import style from '@styles/header/style.module.scss';
+import logoutIcon from '@assets/reload.svg';
+import { Link } from 'react-router-dom';
 
 export default function HeaderInteraction() {
-    return <section>
-
+    return <section className={style.right}>
+        <Link to="/logout" className={style.exit}>
+            <img src={logoutIcon} alt="logout btn" />
+        </Link>
     </section>;
 }
