3bc325cee079c21302615375141c863e4682dc0a
domi <domi@domi.kr>
2025-03-10 14:25:19 +0900

    백엔드 소셜 로그인 토큰 발급

---
+++ server/src/main/java/com/domi/ggmassetbackend/filters/LoginSuccessHandler.java
@@ -0,0 +1,43 @@
+package com.domi.ggmassetbackend.filters;
+
+import com.domi.ggmassetbackend.data.dto.LoginTokenDTO;
+import com.domi.ggmassetbackend.data.entity.PrincipalDetails;
+import com.domi.ggmassetbackend.exceptions.DomiException;
+import com.domi.ggmassetbackend.services.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // 토큰 만들기
+        Object principalBoxing = authentication.getPrincipal();
+        if (!(principalBoxing instanceof PrincipalDetails principalDetails)) {
+            throw new DomiException("LOGIN0", "인증 서버 로그인 오류.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        String email = principalDetails.getUsername();
+        String accessToken = jwtService.generateToken(email, false);
+        String refreshToken = jwtService.generateToken(email, true);
+
+        LoginTokenDTO tokenData = new LoginTokenDTO(accessToken, refreshToken);
+        String result = objectMapper.writeValueAsString(tokenData);
+
+        response.getWriter().write(result);
+    }
+}
