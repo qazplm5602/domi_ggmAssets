6cb499b916bb872ad8f22cff505166f5f938b600
domi <domi@domi.kr>
2025-03-11 11:01:26 +0900

    프론트 로그인 페이지

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,8 +2,15 @@ import style from '@styles/header/style.module.scss';
 import HeaderInteraction from './Interaction';
 import HeaderMain from './Main';
 import SearchBox from '@components/SearchBox/SearchBox';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
+    const location = useLocation();
+
+    // 로그인 화면은 헤더가 안보여야 함
+    if (location.pathname.startsWith("/login"))
+        return;
+    
     return <header className={style.main}>
         <HeaderMain />
         <SearchBox className={style.search} />
--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -4,5 +4,6 @@ import style from '@styles/login/style.module.scss';
 export default function Login() {
     return <main className={style.container}>
         <LoginBox />
+        <div className={style.license}>ⓒ 2025. domi All rights reserved.</div>
     </main>
 }
