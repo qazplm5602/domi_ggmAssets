24e744abc24e2adeb4f25bcf228630c19ae1b842
domi <domi@domi.kr>
2025-03-11 23:32:28 +0900

    프론트 카테고리 컴포넌트

--- client/src/components/AssetsList/Side/CategoryBox.tsx
+++ client/src/components/AssetsList/Side/CategoryBox.tsx
@@ -1,12 +1,21 @@
 import style from '@styles/assetsList/side.module.scss';
+import checkIcon from '@assets/icons/check.svg';
+
+import leftArrowSvg from '@assets/icons/arrow-left.svg';
 
 export default function AssetsListSideCategoryBox() {
     return <>
         <div className={style.category}>
             <input type="checkbox" id='domitest' className={style.check} />
-            <label htmlFor="domitest">3D</label>
+            <label htmlFor="domitest" className={style.check_design}>
+                <img src={checkIcon} draggable={false} alt='check' />
+            </label>
+            <label htmlFor="domitest" className={style.text}>3D</label>
+
             {/* <p>3D</p> */}
-            <button></button>
+            <button className={`${style.toggle} ${style.active}`}>
+                <img src={leftArrowSvg} alt="toggle" />
+            </button>
         </div>
 
         {/* 하위 카테고리까지 선택 ㅁㄴㅇㄹ */}
