7e7b01f07066dcf15e2e88f43c4477a3a8abce80
domi <domi@domi.kr>
2025-03-12 09:23:26 +0900

    프론트 에셋 썸네일 버튼

--- client/src/components/AssetsList/Item/Thumbnail.tsx
+++ client/src/components/AssetsList/Item/Thumbnail.tsx
@@ -7,10 +7,6 @@ export default function AssetsListItemThumbnail() {
             <img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/c0ffba0f-6541-4dc7-9364-67ecb0e41367_scaled.jpg" alt="thumbnail 1" />
         </article>
 
-        {/* <button className={stylze.btn}>
-            <img src={rightArrow} alt="right arrow" />
-        </button> */}
-        
         <AssetsListThumbnailArrowButton />
         <AssetsListThumbnailArrowButton right={true} />
     </section>
