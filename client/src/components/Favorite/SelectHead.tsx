2c8bfb1b397b51cc7f0b35bf4c3ec87bd5228a32
domi <domi@domi.kr>
2025-03-13 19:40:46 +0900

    프론트 like 선택 헤더 컴포넌트

--- client/src/components/Favorite/SelectHead.tsx
+++ client/src/components/Favorite/SelectHead.tsx
@@ -1,15 +1,17 @@
 import Button from '@components/Buttons/Button';
 import style from '@styles/favorite/style.module.scss';
+import FavoriteSelectHeadCheck from './SelectHeadCheck';
 
 export default function FavoriteSelectHead() {
     return <section className={style.selectHead}>
         <article className={style.left}>
+            <FavoriteSelectHeadCheck />
             <div className={style.amount}>0개 선택함</div>
         </article>
         
         <article className={style.right}>
-            <Button>다운로드</Button>
-            <Button>삭제</Button>
+            <Button className={style.download}>다운로드</Button>
+            <Button className={style.remove}>삭제</Button>
         </article>
     </section>
 }
--- client/src/components/Favorite/SelectHeadCheck.tsx
+++ client/src/components/Favorite/SelectHeadCheck.tsx
@@ -1,10 +1,11 @@
-import style from '@styles/assetsList/side.module.scss';
+import checkStyle from '@styles/assetsList/side.module.scss';
 import checkIcon from '@assets/icons/check.svg';
+// import lineIcon from '@assets/icons/horizontal-line.svg';
 
 export default function FavoriteSelectHeadCheck() {
     return <>
-        <input type="checkbox" id='domitest' className={style.check} />
-        <label htmlFor="domitest" className={style.check_design}>
+        <input type="checkbox" id='domitest222' className={checkStyle.check} />
+        <label htmlFor="domitest222" className={checkStyle.check_design}>
             <img src={checkIcon} draggable={false} alt='check' />
         </label>
     </>;
