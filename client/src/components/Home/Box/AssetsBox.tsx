bce0ce01444257d50021b0047a6b3beb9220dd80
domi <domi@domi.kr>
2025-03-13 12:53:34 +0900

    프론트 메인 애니메이션 적용

--- client/src/components/Home/Box/AssetsBox.tsx
+++ client/src/components/Home/Box/AssetsBox.tsx
@@ -2,8 +2,19 @@ import style from '@styles/home/style.module.scss';
 import HomeAssetsBoxHead from './Head';
 import AssetItem from '@components/AssetsList/Item/Item';
 
+import { AnimationProps, motion } from 'framer-motion';
+
+const INIT_STYLE: AnimationProps['initial'] = {
+    y: 50,
+    opacity: 0
+}
+const NOW_STYLE: AnimationProps['animate'] = {
+    y: 0,
+    opacity: 1
+}
+
 export default function HomeAssetsBox() {
-    return <div className={style.box}>
+    return <motion.div initial={INIT_STYLE} animate={NOW_STYLE} className={style.box}>
         <HomeAssetsBoxHead />
         
         {/* 리스트 */}
@@ -15,5 +26,5 @@ export default function HomeAssetsBox() {
             <AssetItem className={style.item} />
             <AssetItem className={style.item} />
         </article>
-    </div>
+    </motion.div>
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -4,10 +4,11 @@ import ggmLogo from '@assets/ggmLow.webp';
 import Head from '../../components/Home/Head';
 import HomeContent from '../../components/Home/Content';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export default function Home() {
     return <main className={style.main}>
-        <img src={ggmLogo} alt="ggm" className={style.bg_blur} />
+        <motion.img src={ggmLogo} alt="ggm" className={style.bg_blur} initial={{opacity: 0}} animate={{opacity: 0.3}} transition={{ duration: 0.5 }} />
         <Head />
 
         <HomeContent />
