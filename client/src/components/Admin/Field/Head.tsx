2e50e7b4030f1ed973d44259ddc04ef8fb9669a3
domi <domi@domi.kr>
2025-03-24 23:33:57 +0900

    프론트 어드민 필드

---
+++ client/src/components/Admin/Field/Head.tsx
@@ -0,0 +1,15 @@
+import style from '@styles/admin/style.module.scss';
+
+type Props = {
+    required?: boolean
+    title: string,
+    desc?: string
+}
+
+export default function AdminFieldHead({ title, desc, required }: Props) {
+    return <section className={style.head}>
+        {required && <div className={style.small}>* 필수</div>}
+        <h3>{title}</h3>
+        <p className={style.desc}>{desc}</p>
+    </section>;
+}
--- client/src/pages/Admin/Upload.tsx
+++ client/src/pages/Admin/Upload.tsx
@@ -1,9 +1,14 @@
+import AdminField from '@components/Admin/Field/Field';
 import AdminHead from '@components/Admin/Head';
+import Input from '@components/Admin/Inputs/Input';
 import style from '@styles/admin/style.module.scss';
 
 export default function AdminUpload() {
     return <main className={style.small_screen}>
         <AdminHead />
         
+        <AdminField title="다운로드 링크" desc='다운로드 버튼을 누르면 설정한 링크로 이동합니다.' required={true}>
+            <Input placeholder='다운로드 링크를 입력하세요.' />
+        </AdminField>
     </main>;
 }
