3eb463c8a0adabe8fe5cb040a3d3ada7b37a98ac
domi <domi@domi.kr>
2025-03-25 00:46:42 +0900

    프론트 업로드 스토어 감지

--- client/src/components/Admin/Field/StoreLinkField.tsx
+++ client/src/components/Admin/Field/StoreLinkField.tsx
@@ -3,8 +3,16 @@ import style from '@styles/admin/upload.module.scss';
 import AdminField from "./Field";
 import Input from "../Inputs/Input";
 
+import storeLogo from '@assets/unity-assets-logo.svg';
+import questionIcon from '@assets/icons/question.svg';
+
 export default function AdminStoreLinkField() {
     return <AdminField title="스토어 링크" desc='해당 URL을 통해 에셋 정보를 분석하고 적용합니다.' className={style.field}>
         <Input placeholder='스토어에 등록되어있는 에셋 링크를 입력하세요.' />
+        
+        <div className={style.detect}>
+            <img src={storeLogo} alt="store logo" />
+            <span>감지됨</span>
+        </div>
     </AdminField>
 }
