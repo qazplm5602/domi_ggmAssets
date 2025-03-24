328ce0b40182dab40fe98076e6eb594a48ca976f
domi <domi@domi.kr>
2025-03-24 20:06:37 +0900

    프론트 관리 메뉴 추가

--- client/src/components/Admin/Menu/Item.tsx
+++ client/src/components/Admin/Menu/Item.tsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import style from '@styles/admin/menu.module.scss';
 
-import uploadIcon from '@assets/icons/upload.svg';
+type Props = {
+    page: string,
+    icon: string,
+    title: string,
+    desc?: string
+}
 
-export default function AdminMenuItem() {
-    return <Link to="/admin/upload">
+export default function AdminMenuItem({ title, desc, icon, page }: Props) {
+    return <Link to={`/admin/${page}`}>
         <div className={style.menu}>
             <div className={style.icon}>
-                <img src={uploadIcon} alt="menu icon" />
+                <img src={icon} alt="menu icon" />
             </div>
 
             <section className={style.detail}>
-                <h2>에셋 등록</h2>
-                <p>목록에 새롭게 에셋을 추가 합니다.</p>
+                <h2>{title}</h2>
+                <p>{desc}</p>
             </section>
         </div>
     </Link>
