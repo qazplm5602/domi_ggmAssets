22b985cea4f2a847a23a53cec14cb8cbf90a7dea
domi <domi@domi.kr>
2025-03-27 11:34:22 +0900

    프론트 다운로드 경로 감지

--- client/src/pages/Admin/Upload.tsx
+++ client/src/pages/Admin/Upload.tsx
@@ -5,12 +5,15 @@ import baseStyle from '@styles/admin/style.module.scss';
 import style from '@styles/admin/upload.module.scss';
 import VersionField from '@components/Admin/Field/VersionField';
 import AdminUploadInteraction from '@components/Admin/UploadInteraction';
+import { useState } from 'react';
 
 export default function AdminUpload() {
+    const [ fileLink, setFileLink ] = useState("");
+
     return <main className={`${baseStyle.small_screen} ${style.main}`}>
         <AdminHead className={style.head} />
         
-        <AdminFileLinkField className={style.field} />
+        <AdminFileLinkField className={style.field} value={[fileLink, setFileLink]} />
         <AdminStoreLinkField className={style.field} />
         <VersionField className={style.field} />
 
