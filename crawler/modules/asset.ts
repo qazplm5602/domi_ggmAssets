a548ddb3653488591ee0161854e38668b1081f60
domi <domi@domi.kr>
2025-03-28 11:35:50 +0900

    크롤러 unity 에셋 스토어 데이터 불러옴

--- crawler/modules/asset.ts
+++ crawler/modules/asset.ts
@@ -1,3 +1,50 @@
 export interface AssetDTO {
     
 }
+
+interface UnityStoreSrp {
+    version: string,
+    types: ('lightweight' | 'hd' | 'standard' | 'custom')[]
+}
+
+interface UnityAssetVersion {
+    id: string,
+    name: string,
+    publishedDate: string
+}
+
+interface UnityPublisher {
+    id: string,
+    name: string,
+}
+
+interface UnityThumbnail {
+    type: 'youtube' | 'screenshot',
+    imageUrl: string,
+    thumbnailUrl: string
+}
+
+interface UnityCategoy {
+    id: string,
+    name: string,
+    slug: string,
+    longName: string
+}
+
+export interface UnityStoreProduct {
+    id: string,
+    name: string,
+    description: string,
+    elevatorPitch: string,
+    srps: UnityStoreSrp[],
+    currentVersion: UnityAssetVersion,
+    downloadSize: string,
+    publisher: UnityPublisher
+    images: UnityThumbnail[],
+    category: UnityCategoy,
+    firstPublishedDate: string,
+    supportedUnityVersions: string[],
+    // state: string,
+    // overlay: string,
+    // overlayText: string
+}
--- crawler/modules/unity.ts
+++ crawler/modules/unity.ts
@@ -1,3 +1,4 @@
+import { UnityStoreProduct } from "./asset.ts";
 import { CrawlerCallbackType, registerPlatformHandler } from "./crawler.ts";
 import { DomiError } from "./error.ts";
 import { getIdByUnityStoreUrl } from "./linkParse.ts";
@@ -10,8 +11,39 @@ const unityAssetDataLoadhandler: CrawlerCallbackType = async function(req) {
     }
 
     const assetId = getIdByUnityStoreUrl(url);
+    
+    const headers: HeadersInit  = {
+        Cookie: "_csrf=domi;NEXT_LOCALE=ko-KR;",
+        Origin: "https://assetstore.unity.com",
+        Referer: "https://assetstore.unity.com/",
+        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6",
+        "X-Csrf-Token": "domi",
+        "X-Requested-with": "XMLHttpRequest",
+        "X-Source": "storefront"
+    };
+    
+    const body = [
+        {
+            "query": "query ProductReview($id: ID!, $rows: Int, $page: Int, $sort_by: String, $reviewId: String, $rating: String) {\n  product(id: $id) {\n    ...product\n    reviews(rows: $rows, page: $page, sortBy: $sort_by, reviewId: $reviewId, rating: $rating) {\n      ...reviews\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment product on Product {\n  id\n  productId\n  itemId\n  slug\n  name\n  description\n  aiDescription\n  elevatorPitch\n  keyFeatures\n  compatibilityInfo\n  srps {\n    version\n    types\n    __typename\n  }\n  rating {\n    average\n    count\n    __typename\n  }\n  currentVersion {\n    id\n    name\n    publishedDate\n    __typename\n  }\n  reviewCount\n  downloadSize\n  assetCount\n  publisher {\n    id\n    name\n    url\n    supportUrl\n    supportEmail\n    gaAccount\n    gaPrefix\n    __typename\n  }\n  mainImage {\n    big\n    facebook\n    small\n    icon\n    icon75\n    __typename\n  }\n  originalPrice {\n    itemId\n    originalPrice\n    finalPrice\n    isFree\n    discount {\n      save\n      percentage\n      type\n      saleType\n      __typename\n    }\n    currency\n    entitlementType\n    __typename\n  }\n  images {\n    type\n    imageUrl\n    thumbnailUrl\n    __typename\n  }\n  category {\n    id\n    name\n    slug\n    longName\n    __typename\n  }\n  firstPublishedDate\n  publishNotes\n  supportedUnityVersions\n  state\n  overlay\n  overlayText\n  popularTags {\n    id\n    pTagId\n    name\n    __typename\n  }\n  plusProSale\n  licenseText\n  vspProperties {\n    ... on ExternalVSPProduct {\n      externalLink\n      __typename\n    }\n    __typename\n  }\n  packageType\n  __typename\n}\n\nfragment reviews on Reviews {\n  count\n  canRate: can_rate\n  canReply: can_reply\n  canComment: can_comment\n  hasCommented: has_commented\n  totalEntries: total_entries\n  lastPage: last_page\n  comments {\n    id\n    date\n    editable\n    rating\n    user {\n      id\n      name\n      profileUrl\n      avatar\n      __typename\n    }\n    isHelpful: is_helpful {\n      count\n      score\n      __typename\n    }\n    subject\n    version\n    full\n    is_complimentary\n    vote\n    replies {\n      id\n      editable\n      date\n      version\n      full\n      user {\n        id\n        name\n        profileUrl\n        avatar\n        __typename\n      }\n      isHelpful: is_helpful {\n        count\n        score\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n",
+            "variables": {
+                "id": assetId.toString(),
+                "rows": 10,
+                "page": 1,
+                "sort_by": "recent",
+                "rating": null
+            },
+            "operationName": "ProductReview"
+        }
+    ];
 
-    return { id: assetId };
+    const result = await fetch("https://assetstore.unity.com/api/graphql/batch", { method: "POST", headers: headers, body: JSON.stringify(body) });
+    if (result.status !== 200)
+        throw new DomiError(500, "unity api error");
+
+    const data = await result.json();
+    const product = data[0].data.product as UnityStoreProduct;
+
+    return product;
 }
 
 registerPlatformHandler("unity", unityAssetDataLoadhandler);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
