e24274c9a67f4d1e1aa8a6b30fcd62770c9b97c4
domi <domi@domi.kr>
2025-03-29 17:06:28 +0900

    크롤러 exception 처리

--- crawler/modules/route.ts
+++ crawler/modules/route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { getPlatformHandler } from './crawler.ts';
+import { DomiError } from './error.ts';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,8 +17,16 @@ app.post("/asset/:platform", async function(req, res) {
         return;
     }
 
-    const response = await callback(req);
-    res.send(response);
+    try {
+        const response = await callback(req);
+        res.send(response);
+    } catch (e) {
+        if (!(e instanceof DomiError))
+            throw e;
+
+        const error = e as DomiError;
+        res.status(error.status).send(error.message);
+    }
 });
 
 app.listen(process.env.API_PORT, () => console.log(`${process.env.API_PORT} listen!`));
