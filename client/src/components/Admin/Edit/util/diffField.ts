3a885e0ff095c80af7b90f700acfc71f1abeff3e
domi <domi@domi.kr>
2025-04-08 17:45:40 +0900

    프론트 에셋 수정 등록일 비교

--- client/src/components/Admin/Edit/util/diffField.ts
+++ client/src/components/Admin/Edit/util/diffField.ts
@@ -13,6 +13,21 @@ export function hasAssetEditFieldUpdated(fields: AssetEditFieldStates, origin: A
     if (fields.platform[0] !== origin.platform) return true;
     if (!isOtherField(fields.artist, origin.publisher)) return true;
 
+    // 등록일 비교
+    if ((fields.createAt[0] !== "" &&  origin.publishAt === null) || (fields.createAt[0] === "" && origin.publishAt !== null)) {
+        return true;
+    }
+    
+    // 등록일이 똑같은지 바교 ㅁㄴㅇㄹ
+    if (fields.createAt[0] !== "" && origin.publishAt !== null) {
+        const originDate = new Date(origin.publishAt);
+        const currentDate = new Date(fields.createAt[0]);
+        
+        // 날짜가 다르네잉
+        if (originDate.getFullYear() !== currentDate.getFullYear() || originDate.getMonth() !== currentDate.getMonth() || originDate.getDate() !== currentDate.getDate())
+            return true;
+    }
+
     return false;
 }
 
