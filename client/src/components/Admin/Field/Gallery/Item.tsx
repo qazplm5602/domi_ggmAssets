d17fb28da5de8dcc5b2715b1277066671c63ef3f
domi <domi@domi.kr>
2025-04-09 23:52:23 +0900

    프론트 에셋 수정 썸네일 이동, 삭제

--- client/src/components/Admin/Field/Gallery/Item.tsx
+++ client/src/components/Admin/Field/Gallery/Item.tsx
@@ -6,22 +6,29 @@ import { ThumbnailLocalVO } from '@domiTypes/assetEdit';
 import { getThumbnailURL } from '@utils/file';
 
 type Props = {
-    data: ThumbnailLocalVO
+    data: ThumbnailLocalVO,
+    onMove?: (left: boolean) => void,
+    onRemove?: () => void
 }
 
-export default function AdminEditGalleryItem({ data }: Props) {
+export default function AdminEditGalleryItem({ data, onMove, onRemove }: Props) {
+    const handleMove = function(left: boolean) {
+        if (onMove)
+            onMove(left);
+    }
+    
     return <div className={style.box}>
         <img src={getThumbnailURL(data.type === 'Youtube' ? data.previewUrl : data.contentUrl)} alt="preview image" className={style.preview} />
 
         {/* 관리 그거 */}
         <div className={style.option}>
-            <button>
+            <button onClick={() => handleMove(true)}>
                 <img src={leftArrow} alt="left arrow" />
             </button>
-            <button>
+            <button onClick={onRemove}>
                 <img src={deleteIcon} alt="remove arrow" />
             </button>
-            <button>
+            <button onClick={() => handleMove(false)}>
                 <img src={leftArrow} alt="right arrow" className={style.flip} />
             </button>
         </div>
--- client/src/components/Admin/Field/ImageField.tsx
+++ client/src/components/Admin/Field/ImageField.tsx
@@ -12,10 +12,24 @@ type Props = {
 }
 
 export default function AdminEditImageField({ className, state: [ images, setImages ], onAdd }: Props) {
+    const handleItemMove = function(idx: number, left: boolean) {
+        const newImages = [ ...images ];
+        const item = newImages.splice(idx, 1);
+
+        newImages.splice(idx + (left ? -1 : 1), 0, ...item);
+        setImages(newImages);
+    }
+    const handleItemRemove = function(idx: number) {
+        const newImages = [ ...images ];
+        newImages.splice(idx, 1);
+        
+        setImages(newImages);
+    }
+
     return <AdminField title="썸네일" className={`${style.imageField} ${className || ''}`}>
         <section className={style.gallery}>
             <AdminEditGalleryAddBox onClick={onAdd} />
-            {images.map((v, i) => <AdminEditGalleryItem key={i} data={v} />)}
+            {images.map((v, i) => <AdminEditGalleryItem key={i} data={v} onMove={left => handleItemMove(i, left)} onRemove={() => handleItemRemove(i)} />)}
             {/* <AdminEditGalleryItem />
             <AdminEditGalleryItem /> */}
         </section>
