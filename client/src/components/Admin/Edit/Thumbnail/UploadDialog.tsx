bff553302f1b94aee9731ba3e0488977368e0210
domi <domi@domi.kr>
2025-04-09 19:29:25 +0900

    프론트 썸네일 업로드 팝업

--- client/src/components/Admin/Edit/Thumbnail/UploadDialog.tsx
+++ client/src/components/Admin/Edit/Thumbnail/UploadDialog.tsx
@@ -1,9 +1,11 @@
 import style from '@styles/admin/edit.module.scss';
 import Dialog from "@components/Dialog/Dialog";
 import AdminEditThumbnailUploadDialogSelect from './Select';
+import AdminEditThumbnailUploadDialogYoutube from './Youtube';
 
 export default function AdminEditThumbnailUploadDialog() {
     return <Dialog title="썸네일 업로드" show={true} className={style.thumbnailUploadDialog}>
-        <AdminEditThumbnailUploadDialogSelect />
+        {/* <AdminEditThumbnailUploadDialogSelect /> */}
+        <AdminEditThumbnailUploadDialogYoutube />
     </Dialog>
 }
---
+++ client/src/components/Admin/Edit/Thumbnail/Youtube.tsx
@@ -0,0 +1,18 @@
+import AdminField from '@components/Admin/Field/Field';
+import Input from '@components/Admin/Inputs/Input';
+import Button from '@components/Buttons/Button';
+import style from '@styles/admin/edit.module.scss';
+
+export default function AdminEditThumbnailUploadDialogYoutube() {
+    return <section className={`${style.content} ${style.youtube}`}>
+        <AdminField title="유튜브 URL" className={style.field}>
+            <Input placeholder='유튜브 영상 링크를 입력하세요.' />
+            <div className={style.alert}>유튜브 영상 링크나 ID가 아닙니다.</div>
+        </AdminField>
+
+        <article className={style.interaction}>
+            <Button>뒤로가기</Button>
+            <Button className={style.upload}>업로드</Button>
+        </article>
+    </section>;
+}
