5a9c9918c016b162e658751313a21cd46815e328
domi <domi@domi.kr>
2025-04-10 09:59:39 +0900

    프론트 썸네일 업로드 파일 선택

--- client/src/components/Admin/Edit/Thumbnail/UploadDialog.tsx
+++ client/src/components/Admin/Edit/Thumbnail/UploadDialog.tsx
@@ -2,7 +2,8 @@ import style from '@styles/admin/edit.module.scss';
 import Dialog from "@components/Dialog/Dialog";
 import AdminEditThumbnailUploadDialogSelect from './Select';
 import AdminEditThumbnailUploadDialogYoutube from './Youtube';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { usePopupStore } from '@components/Popup/store';
 
 type Props = {
     show: boolean,
@@ -11,9 +12,36 @@ type Props = {
 
 export default function AdminEditThumbnailUploadDialog({ show, onClose }: Props) {
     const [ screen, setScreen ] = useState<'select' | 'youtube'>('select');
+    const fileRef = useRef<HTMLInputElement>(null);
+    const { openPopup } = usePopupStore();
 
     const handleSelectImage = function() {
-         
+        fileRef.current?.click();
+    }
+    
+    const handleChangeInputFile = function(e: React.ChangeEvent<HTMLInputElement>) {
+        const files = e.target.files;
+        if (!files) return; // 머가 없는디
+        
+        let verify = true;
+
+        // 여러 파일 지원
+        for (let i = 0; i < files.length; i++) {
+            const file = files.item(i);
+            if (file === null) continue;
+            
+            if (!file.type.startsWith("image/")) {
+                verify = false;
+                break;
+            }
+        }
+
+        if (!verify) {
+            openPopup("업로드 오류", "이미지 파일만 가능합니다.", [ { text: "확인", callback() {} } ]);
+            return;
+        }
+
+        console.log(files);
     }
     
     const handleSelectYoutube = function() {
@@ -27,5 +55,8 @@ export default function AdminEditThumbnailUploadDialog({ show, onClose }: Props)
     return <Dialog title="썸네일 업로드" show={show} className={style.thumbnailUploadDialog} onClose={onClose}>
         {screen === 'select' && <AdminEditThumbnailUploadDialogSelect onSelectImage={handleSelectImage} onSelectYoutube={handleSelectYoutube} />}
         {screen === 'youtube' && <AdminEditThumbnailUploadDialogYoutube onBack={handleBackScreen} />}
+
+        {/* 이미지 파일 넣는곳 */}
+        <input type="file" style={{ display: "none" }} ref={fileRef} onChange={handleChangeInputFile} accept='image/*' multiple />
     </Dialog>
 }
