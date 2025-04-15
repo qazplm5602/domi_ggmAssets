63fcd27490d2349597bcda74e9130d0fb48e2e91
domi <domi@domi.kr>
2025-04-16 01:54:34 +0900

    크롤러 itchio 정보 파싱

--- crawler/modules/itchio.ts
+++ crawler/modules/itchio.ts
@@ -1,3 +1,4 @@
+import * as cheerio from "cheerio";
 import { AssetDTO } from "./asset.ts";
 import { CrawlerCallbackType, registerPlatformHandler } from "./crawler.ts";
 import { getItchioOwnerHost } from "./linkParse.ts";
@@ -6,9 +7,22 @@ const itchioDataLoadHandler: CrawlerCallbackType = async function(req) {
     const itchUrl = req.body.url;
     const ownerUrl = getItchioOwnerHost(itchUrl);
 
-    
+    const response = await fetch(itchUrl);
+    const html = await response.text();
+    const $ = cheerio.load(html);
 
-    return {} as AssetDTO; // 아직 머가 없엉 (테스트)
+    const title = $(".game_title").text();
+    const desc = $(".formatted_description.user_formatted").html();
+    const shortDesc = $("meta[property=\"og:description\"]").attr("content");
+    const publisher = $(".on_follow > .full_label").text().substring(("Follow ").length);
+
+
+    return {
+        title,
+        desc,
+        shortDesc,
+        publisher
+    } as AssetDTO; // 아직 머가 없엉 (테스트)
 }
 
 registerPlatformHandler("itchio", itchioDataLoadHandler);
