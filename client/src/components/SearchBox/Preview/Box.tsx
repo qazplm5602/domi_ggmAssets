f55d8564aab4b7c10dcce653ecdb55ac52535fbd
domi <domi@domi.kr>
2025-04-15 17:38:23 +0900

    프론트 검색 프리뷰 빈 아이콘 표시

--- client/src/components/SearchBox/Preview/Box.tsx
+++ client/src/components/SearchBox/Preview/Box.tsx
@@ -1,6 +1,9 @@
 import { AssetSearchVO } from '@domiTypes/asset';
 import style from '@styles/searchBox/style.module.scss';
+import { getThumbnailURL } from '@utils/file';
 import { Link } from 'react-router-dom';
+
+import emptyIcon from '@assets/empty-icon.webp';
 
 type Props = {
     data: AssetSearchVO
@@ -9,7 +12,7 @@ type Props = {
 export default function SearchPreviewBox({ data }: Props) {
     return <Link to={`/asset/${data.id}`}>
         <div className={style.item}>
-            <img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/080f519a-6d10-4d3e-b83f-31dd8b9e9319.jpg" alt="thumbnail" className={style.thumbnail} />
+            <img src={data.thumbnail ? getThumbnailURL(data.thumbnail) : emptyIcon} alt="thumbnail" className={style.thumbnail} />
             <div className={style.detail}>
                 <h3>{data.title}</h3>
                 <p>{data.category ? data.category.join(" / ") : "분류되지 않음"}</p>
