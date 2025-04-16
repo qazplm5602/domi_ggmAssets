9467e57c6b9ebe1af243ccd2d3478a5393d0bd25
domi <domi@domi.kr>
2025-04-17 00:41:53 +0900

    프론트 삭제 물어보깅

--- client/src/components/Favorite/SelectHead.tsx
+++ client/src/components/Favorite/SelectHead.tsx
@@ -3,6 +3,7 @@ import style from '@styles/favorite/style.module.scss';
 import FavoriteSelectHeadCheck from './SelectHeadCheck';
 import { formatNumberWithCommas } from '@utils/misc';
 import { AssetPreviewVO } from '@domiTypes/asset';
+import { usePopupStore } from '@components/Popup/store';
 
 type Props = {
     selects: Set<number>,
@@ -12,7 +13,15 @@ type Props = {
 
 export default function FavoriteSelectHead({ selects, assets, onCheck }: Props) {
     const allChecked = assets !== null &&  assets.length > 0 && selects.size === assets.length;
+    const { openPopup, closePopup } = usePopupStore();
 
+    const handleRemove = function() {
+        openPopup("삭제하시겠어요?", <p>찜한 에셋 {formatNumberWithCommas(selects.size)}개를 삭제하시겠습니까?</p>, [
+                { text: "삭제", color: "#D44760", callback() {} },
+                { text: "취소", callback: closePopup, }
+        ]);
+    }
+    
     return <section className={style.selectHead}>
         <article className={style.left}>
             <FavoriteSelectHeadCheck all={allChecked} active={allChecked || selects.size > 0} onChange={onCheck} />
@@ -21,7 +30,7 @@ export default function FavoriteSelectHead({ selects, assets, onCheck }: Props) 
         
         <article className={style.right}>
             <Button className={style.download}>다운로드</Button>
-            <Button className={style.remove}>삭제</Button>
+            <Button className={style.remove} onClick={handleRemove}>삭제</Button>
         </article>
     </section>
 }
