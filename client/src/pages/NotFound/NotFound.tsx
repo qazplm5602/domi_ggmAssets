5ca7b1ce56f61f5a64bb9e6cdf8031d5ee3c9c40
domi <domi@domi.kr>
2025-04-17 20:04:42 +0900

    프론트 notfound 이미지 추가

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,11 @@
 import style from '@styles/notFound/style.module.scss';
 import { Link } from 'react-router-dom';
 
+import suhansNotFoundIcon from '@assets/KLDP-notfound.webp';
+
 export default function NotFound() {
     return <main className={style.main}>
-        <img src="" alt='not found' />
+        <img src={suhansNotFoundIcon} alt='not found' />
         <p>해당 페이지는 없거나<span>(머리 아님)</span> 삭제되었습니다.</p>
         
         <Link to="/" className={style.home}>홈으로</Link>
