7c41afad642b524ec7d9a129a7263a1039ab7ff2
domi <domi@domi.kr>
2025-04-22 00:50:44 +0900

    프론트 자동 채우기 전체 체크 반영

--- client/src/components/Admin/Edit/AutoField/Option.tsx
+++ client/src/components/Admin/Edit/AutoField/Option.tsx
@@ -2,17 +2,20 @@ import CheckBoxContainer from '@components/CheckBox/Container';
 import style from '@styles/admin/edit.module.scss';
 import AdminEditAutoFieldDialogOptionRow from './Row';
 import { AutoFieldCheckState } from '@domiTypes/assetAutoField';
+import { useMemo } from 'react';
 
 type Props = {
     checkStates: AutoFieldCheckState
 }
 
 export default function AdminEditAutoFieldDialogOption({ checkStates }: Props) {
+    const allChecked = useMemo(() => Object.values(checkStates).every(v => v[0]), [ checkStates ]);
+
     return <article className={style.option}>
         <h3>옵션</h3>
 
         <section className={style.all}>
-            <CheckBoxContainer id='autoField-All' name='전체' />
+            <CheckBoxContainer id='autoField-All' name='전체' checked={allChecked} />
         </section>
 
         <AdminEditAutoFieldDialogOptionRow checkStates={checkStates} />
