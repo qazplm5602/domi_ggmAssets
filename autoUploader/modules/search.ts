c776f822c7f2fd143ba55b9cb513fd6b58203c33
domi <domi@domi.kr>
2025-04-24 11:26:04 +0900

    자동 업로더 bing url 오류 고침

--- autoUploader/modules/search.ts
+++ autoUploader/modules/search.ts
@@ -9,33 +9,46 @@ export async function findStoreURL(name: string, platform: AssetFileItem['platfo
     // page.close();
 
     // 생각 해보니 bing 은 브라우저로 안열어도 됨 ㄷㄷ
-    const query = `q=${name}&pq=${name}`;
-    const result = await fetch(`https://www.bing.com/search?${encodeURI(query)}`, { headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Whale/4.31.304.16 Safari/537.36" } });
+    const query = `q=${encodeURIComponent(name)}&pq=${encodeURIComponent(name)}`;
+    const result = await fetch(`https://www.bing.com/search?${query}`, { headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Whale/4.31.304.16 Safari/537.36" } });
     const html = await result.text();
     const $ = cheerio.load(html);
 
     let findUrl: string | null = null;
 
     $(".b_algo h2 > a").each((idx, el) => {
-        const url = $(el).attr("href");
+        let url = $(el).attr("href");
         if (!url) return;
 
         const urlParse = URL.parse(url);
         if (!urlParse) return;
         
         if (urlParse.host.endsWith(acceptDomain)) {
-            console.log(`${name} check.`, url);
+            // console.log(`${name} check.`, url);
 
             // itch io 결제 페이지는 제외 해야함ㅁㅁㅁㅁㅁ
-            if (platform === 'Itchio' && urlParse.pathname.split("/").length > 2) { // 경로가 왜 더 있냠
-                console.log(`${name} nope.`);
-                return;
+            const uriList = urlParse.pathname.split("/");
+            if (platform === 'Itchio' && uriList.length > 2) { // 경로가 왜 더 있냠
+                // console.log(`${name} nope.`);
+                
+                if (uriList[uriList.length - 1] !== "purchase")
+                    return;
+
+                // 맨 끝에 삭제 하고
+                uriList.splice(uriList.length - 1, 1);
+                urlParse.pathname = uriList.join("/");
+
+                url = urlParse.toString();
             }
 
             findUrl = url;
             return false; // each 그만~~~
         }
     });
+
+    // if (!findUrl)  {
+    //     console.log("검색 기록 없음", name, html);
+    // }
 
     return findUrl;
 }
