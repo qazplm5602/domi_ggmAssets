fc95ae9a19f6f6946ca7484538e8729454e3e735
domi <domi@domi.kr>
2025-04-29 17:46:35 +0900

    프론트 로그인 배경 넣응

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,8 +1,11 @@
 import LoginBox from '@components/LoginBox/LoginBox';
 import style from '@styles/login/style.module.scss';
 
+import sirokoBg from '@assets/gamebg/sunaookami-shiroko.webp';
+
 export default function Login() {
     return <main className={style.container}>
+        <img src={sirokoBg} className={style.background} alt='login' />
         <LoginBox />
         <div className={style.license}>ⓒ 2025. domi All rights reserved.</div>
     </main>
