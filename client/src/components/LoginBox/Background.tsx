df71129346e2e17dd6e61b05647ec6262d562fcf
domi <domi@domi.kr>
2025-04-29 19:55:00 +0900

    #21 프론트 로그인 창 배경 더 추가

--- client/src/components/LoginBox/Background.tsx
+++ client/src/components/LoginBox/Background.tsx
@@ -1,5 +1,6 @@
 import style from '@styles/login/style.module.scss';
 import { useState } from 'react';
+import { generateRandomNumber } from '@utils/misc';
 
 import sirokoBg from '@assets/gamebg/sunaookami-shiroko.webp';
 import sannabiBg from '@assets/gamebg/sanabi.webp';
@@ -7,8 +8,13 @@ import dungeonAndFighterBg from '@assets/gamebg/DungeonAndFighter.webp';
 import genshinImpactBg from '@assets/gamebg/genshin-impact.webp';
 import limbusBg from '@assets/gamebg/wp12370192-limbus-company-wallpapers.webp';
 import ZZZBg from '@assets/gamebg/ZenlessZoneZero.webp';
-import { generateRandomNumber } from '@utils/misc';
-
+import cyber2007Bg from '@assets/gamebg/cyber2007.webp';
+import forzaHorizonBg from '@assets/gamebg/forza-horizon-5.webp';
+import itTakesTwoBg from '@assets/gamebg/it-takes-two.webp';
+import WallFlareBg from '@assets/gamebg/wallpaperflare.webp';
+import minecraftBg from '@assets/gamebg/minecraft.webp';
+import soulknightBg from '@assets/gamebg/soulknight.webp';
+import guiltygearBg from '@assets/gamebg/guiltygear.webp';
 
 const IMAGE_LIST = [
     sirokoBg,
@@ -16,8 +22,15 @@ const IMAGE_LIST = [
     dungeonAndFighterBg,
     genshinImpactBg,
     limbusBg,
-    ZZZBg
-]
+    ZZZBg,
+    cyber2007Bg,
+    forzaHorizonBg,
+    itTakesTwoBg,
+    WallFlareBg,
+    minecraftBg,
+    soulknightBg,
+    guiltygearBg
+];
 
 export default function LoginBackground() {
     const [ loaded, setLoaded ] = useState(false);
