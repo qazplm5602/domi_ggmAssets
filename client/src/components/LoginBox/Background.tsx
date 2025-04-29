2cc1eeb9d939b8d3b08846ca06df65d935fcf700
domi <domi@domi.kr>
2025-04-29 19:24:06 +0900

    #21 프론트 로그인 창 랜덤 배경

--- client/src/components/LoginBox/Background.tsx
+++ client/src/components/LoginBox/Background.tsx
@@ -1,15 +1,31 @@
 import style from '@styles/login/style.module.scss';
+import { useState } from 'react';
 
 import sirokoBg from '@assets/gamebg/sunaookami-shiroko.webp';
 import sannabiBg from '@assets/gamebg/sanabi.webp';
-import { useState } from 'react';
+import dungeonAndFighterBg from '@assets/gamebg/DungeonAndFighter.webp';
+import genshinImpactBg from '@assets/gamebg/genshin-impact.webp';
+import limbusBg from '@assets/gamebg/wp12370192-limbus-company-wallpapers.webp';
+import ZZZBg from '@assets/gamebg/ZenlessZoneZero.webp';
+import { generateRandomNumber } from '@utils/misc';
+
+
+const IMAGE_LIST = [
+    sirokoBg,
+    sannabiBg,
+    dungeonAndFighterBg,
+    genshinImpactBg,
+    limbusBg,
+    ZZZBg
+]
 
 export default function LoginBackground() {
     const [ loaded, setLoaded ] = useState(false);
+    const [ idx ] = useState(generateRandomNumber(0, IMAGE_LIST.length - 1));
 
     const handleLoad = function() {
         setLoaded(true);
     }
 
-    return <img src={sannabiBg} className={`${style.background} ${loaded ? '' : style.hide}`} onLoad={handleLoad} alt='login' />;
+    return <img src={IMAGE_LIST[idx]} className={`${style.background} ${loaded ? '' : style.hide}`} onLoad={handleLoad} alt='login' />;
 }
