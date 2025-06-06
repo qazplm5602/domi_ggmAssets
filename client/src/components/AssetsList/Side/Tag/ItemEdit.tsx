bc2f1603a7cf2a5b7f9536537642a63d086ed080
domi <domi@domi.kr>
2025-06-06 18:42:37 +0900

    프론트 태그 수정 아이템 ㅁㄴㅇㄹ

--- client/src/components/AssetsList/Side/Tag/ItemEdit.tsx
+++ client/src/components/AssetsList/Side/Tag/ItemEdit.tsx
@@ -2,10 +2,12 @@ import Input from '@components/Admin/Inputs/Input';
 import IconButton from '@components/Buttons/IconButton';
 import style from '@styles/assetsList/side.module.scss';
 
+import removeIcon from '@assets/icons/close.svg';
+
 export default function AssetsListSideTagItemEdit() {
     return <div className={`${style.tag} ${style.edit}`}>
-        <div className={style.color} />
-        <Input placeholder='이름을 입력하세요.' />
-        <IconButton icon='' />
+        <button className={style.color} />
+        <Input placeholder='태그 이름' />
+        <IconButton icon={removeIcon} className={style.remove} />
     </div>;
 }
--- client/src/components/AssetsList/Side/Tag/ListEdit.tsx
+++ client/src/components/AssetsList/Side/Tag/ListEdit.tsx
@@ -3,5 +3,9 @@ import AssetsListSideTagItemEdit from "./ItemEdit";
 export default function AssetsListSideTagListEdit() {
     return <>
         <AssetsListSideTagItemEdit />
+        <AssetsListSideTagItemEdit />
+        <AssetsListSideTagItemEdit />
+        <AssetsListSideTagItemEdit />
+        <AssetsListSideTagItemEdit />
     </>;
 }
