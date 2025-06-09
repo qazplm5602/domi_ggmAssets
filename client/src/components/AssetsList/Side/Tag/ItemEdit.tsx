49add1f409a3a2c6e2767473e44871693039b75f
domi <domi@domi.kr>
2025-06-09 20:03:06 +0900

    #26 태그 편집 아이템 연동

--- client/src/components/AssetsList/Side/Tag/ItemEdit.tsx
+++ client/src/components/AssetsList/Side/Tag/ItemEdit.tsx
@@ -4,13 +4,21 @@ import style from '@styles/assetsList/side.module.scss';
 
 import removeIcon from '@assets/icons/close.svg';
 import AssetsListSideTagColorPicker from './ColorPicker';
+import { FavoriteTagVO } from '@domiTypes/favoriteTag';
 
-export default function AssetsListSideTagItemEdit() {
+type Props = {
+    data: FavoriteTagVO
+    onChangeColor?: () => void,
+    onChangeName?: () => void,
+    onRemove?: () => void,
+}
+
+export default function AssetsListSideTagItemEdit({ data, onChangeColor, onChangeName, onRemove }: Props) {
     return <div className={`${style.tag} ${style.edit}`}>
-        <button className={style.color}>
+        <button className={style.color} style={{ backgroundColor: `#${data.color}` }}>
             <AssetsListSideTagColorPicker show={false} />
         </button>
-        <Input placeholder='태그 이름' />
-        <IconButton icon={removeIcon} className={style.remove} />
+        <Input placeholder='태그 이름' value={data.name} />
+        <IconButton icon={removeIcon} className={style.remove} onClick={onRemove} />
     </div>;
 }
--- client/src/components/AssetsList/Side/Tag/ListEdit.tsx
+++ client/src/components/AssetsList/Side/Tag/ListEdit.tsx
@@ -1,11 +1,23 @@
+import { useFavoriteTagList } from "@components/Favorite/Tag/Context";
 import AssetsListSideTagItemEdit from "./ItemEdit";
+import { useEffect, useState } from "react";
+import { FavoriteTagVO } from "@domiTypes/favoriteTag";
 
 export default function AssetsListSideTagListEdit() {
+    const [ originTags ] = useFavoriteTagList(); // 진짜 적용되어있는거
+    const [ localTags, setLocalTags ] = useState<FavoriteTagVO[] | null>(null); // 이건 수정 사항
+
+    useEffect(() => {
+        // 로딩 끗!!!!!
+        if (localTags === null && originTags !== null)
+            setLocalTags([ ...originTags ]);
+    }, [ originTags ]);
+
+    // 왜 없음
+    if (localTags === null)
+        return null;
+
     return <>
-        <AssetsListSideTagItemEdit />
-        <AssetsListSideTagItemEdit />
-        <AssetsListSideTagItemEdit />
-        <AssetsListSideTagItemEdit />
-        <AssetsListSideTagItemEdit />
+        {localTags.map(v => <AssetsListSideTagItemEdit key={v.id} data={v} />)}
     </>;
 }
