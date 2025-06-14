d2d79a18cc12073c9dbe07fa5ed407c503119dcc
domi <domi@domi.kr>
2025-06-14 19:43:51 +0900

    #26 태그 저장 db

---
+++ server/src/main/java/com/domi/ggmassetbackend/data/entity/AssetFavoriteTag.java
@@ -0,0 +1,18 @@
+package com.domi.ggmassetbackend.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@IdClass(AssetFavoriteTagId.class)
+public class AssetFavoriteTag {
+    @Id
+    @ManyToOne
+    private AssetFavorite favorite;
+
+    @Id
+    @ManyToOne
+    private FavoriteTag tag;
+}
---
+++ server/src/main/java/com/domi/ggmassetbackend/data/entity/AssetFavoriteTagId.java
@@ -0,0 +1,8 @@
+package com.domi.ggmassetbackend.data.entity;
+
+import java.io.Serializable;
+
+public class AssetFavoriteTagId implements Serializable {
+    private AssetFavoriteId favorite;
+    private String tag;
+}
