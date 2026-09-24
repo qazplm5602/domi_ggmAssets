009da29d8be7f027157f1bcb8606c804fca2f298
domi <domi@domi.kr>
2026-09-25 00:27:51 +0900

    프론트엔드 sentry 에러 모니터링 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react';
 import '@styles/main/style.scss';
 import App from '@pages/App/App';
 
-createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  release: import.meta.env.VITE_SENTRY_RELEASE,
+  environment: import.meta.env.MODE,
+});
+
+createRoot(document.getElementById('root')!, {
+  onUncaughtError: Sentry.reactErrorHandler(),
+  onCaughtError: Sentry.reactErrorHandler(),
+  onRecoverableError: Sentry.reactErrorHandler(),
+}).render(
   <StrictMode>
     <App />
   </StrictMode>,
