847a7dfcd9c0ea160393af7434dbf5bf6b00a00e
domi <domi@domi.kr>
2026-09-25 00:50:54 +0900

    프론트엔드 sentry 소스맵 업로드 스크립트 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: './env',
+  build: {
+    sourcemap: 'hidden',
+  },
   server: {
     proxy: {
       "/api": {
