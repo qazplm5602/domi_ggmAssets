3c62a38aa7b5a8da0a6bc3ef240ffb44df06a1ad
domi <domi@domi.kr>
2026-09-24 23:52:07 +0900

    썸네일 다운로드 실패 및 시간 초과 sentry 보고

--- server/src/main/java/com/domi/ggmassetbackend/exceptions/ThumbnailException.java
+++ server/src/main/java/com/domi/ggmassetbackend/exceptions/ThumbnailException.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 public class ThumbnailException extends DomiException {
     public enum Type {
         NOT_FOUND(0, "썸네일을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-        NOT_FOUND_ATTACHMENT(1, "찾을 수 없는 핸들 id 입니다.", HttpStatus.NOT_FOUND),;
+        NOT_FOUND_ATTACHMENT(1, "찾을 수 없는 핸들 id 입니다.", HttpStatus.NOT_FOUND),
+        DOWNLOAD_TIMEOUT(2, "썸네일 다운로드 시간이 초과되었습니다.", HttpStatus.INTERNAL_SERVER_ERROR),;
 
         final int code;
         final String message;
@@ -20,5 +21,9 @@ public class ThumbnailException extends DomiException {
 
     public ThumbnailException(Type type) {
         super("THUMBNAIL"+type.code, type.message, type.status);
+    }
+
+    public ThumbnailException(Type type, Throwable cause) {
+        super("THUMBNAIL"+type.code, type.message, type.status, cause);
     }
 }
